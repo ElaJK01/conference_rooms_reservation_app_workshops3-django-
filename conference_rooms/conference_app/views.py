e3ad616218bb4e2e0f108bef384b4da11d05d6da
Elżbieta Janus-Kaleta <adres email>
2020-07-11 23:35:35 +0200

    conference room edition view

--- conference_rooms/conference_app/views.py
+++ conference_rooms/conference_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from conference_app.models import Room
+from datetime import datetime
+from django.shortcuts import redirect
 # Create your views here.
 
 class AddRoom(View):
@@ -38,6 +40,32 @@ class RoomsList(View):
             rooms = rooms.filter(has_projector=True)
         ctx = {'rooms': rooms}
         return render(request, 'rooms_list.html', ctx)
+
+
+class RoomModify(View):
+    def get(self, request, id):
+        if id is None:
+            return HttpResponse('Nie ma takiej sali')
+        else:
+            room = Room.objects.get(id=id)
+            ctx = {"room": room,
+                   }
+        return render(request, "room_modify.html", ctx)
+
+    def post(self,request, id):
+        id = request.POST.get('id')
+        name = request.POST.get('name')
+        capacity = request.POST.get('capacity')
+        has_projector = 'projector' in request.POST
+        if name and capacity:
+            Room.objects.filter(pk=id).update(name=name, capacity=capacity, has_projector=has_projector)
+            return redirect('rooms_list')
+        else:
+            room = Room.objects.get(id=id)
+            ctx = { 'room': room,
+                    'msg': "Wypełnij wszystkie pola"}
+            return render(request, "room_modify.html", ctx)
+
 
 class TempView(View):
     pass
--- conference_rooms/conference_rooms/urls.py
+++ conference_rooms/conference_rooms/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from conference_app.views import AddRoom, RoomsList, TempView
+from conference_app.views import AddRoom, RoomsList, RoomModify, TempView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('room/new/', AddRoom.as_view()),
-    path('room/modify/<int:id>/', TempView.as_view()),
+    path('room/new/', AddRoom.as_view(), name='new_room'),
+    path('room/modify/<int:id>/', RoomModify.as_view(), name='room_modify'),
     path('room/delete/<int:id>/', TempView.as_view()),
     path('room/<int:id>/', TempView.as_view()),
-    path('', RoomsList.as_view()),
+    path('', RoomsList.as_view(), name ='rooms_list'),
 ]
